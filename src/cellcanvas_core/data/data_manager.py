0ca6ea251b6c0b24271babfd5226f1bdd33e7ebf
Kyle Harrington <czi@kyleharrington.com>
2024-06-10 11:03:23 -0400

    Migrate data_set, data_manager, segmentation_manager
    Co-authored-by: Kevin Yamauchi <kevin.yamauchi@gmail.com>
    Co-authored-by: LorenzLamm <34575029+LorenzLamm@users.noreply.github.com>

---
+++ src/cellcanvas_core/data/data_manager.py
@@ -0,0 +1,57 @@
+from typing import List, Optional, Tuple
+
+import dask.array as da
+from cellcanvas.data.data_set import DataSet
+from napari.utils.events.containers import SelectableEventedList
+from zarr import Array
+
+
+class DataManager:
+    def __init__(self, datasets: Optional[List[DataSet]] = None):
+        if datasets is None:
+            datasets = []
+        elif isinstance(datasets, DataSet):
+            datasets = [datasets]
+        self.datasets = SelectableEventedList(datasets)
+
+    # Normal version
+    def get_training_data(self) -> Tuple[Array, Array]:
+        """Get the pixel-wise semantic segmentation training data for datasets.
+
+        Returns
+        -------
+        features : Array
+            (n, d) array of features where n is the number of pixels
+            and d is the number feature dimensions.
+        labels : Array
+            (n,) array of labels for each feature.
+        """
+        features = []
+        labels = []
+        for dataset in self.datasets:
+            dataset_features = da.asarray(dataset.concatenated_features)
+            dataset_labels = da.asarray(dataset.labels)
+            # Flatten labels for boolean indexing
+            flattened_labels = dataset_labels.flatten()
+
+            # Compute valid_indices based on labels > 0
+            valid_indices = da.nonzero(flattened_labels > 0)[0].compute()
+
+            # Flatten only the spatial dimensions of the dataset_features while preserving the feature dimension
+            c, h, w, d = dataset_features.shape
+            reshaped_features = dataset_features.reshape(c, h * w * d)
+
+            # We need to apply valid_indices for each feature dimension separately
+            filtered_features_list = [
+                da.take(reshaped_features[i, :], valid_indices, axis=0)
+                for i in range(c)
+            ]
+            filtered_features = da.stack(filtered_features_list, axis=1)
+
+            # Adjust labels
+            filtered_labels = flattened_labels[valid_indices] - 1
+
+            features.append(filtered_features)
+            labels.append(filtered_labels)
+
+        return da.concatenate(features), da.concatenate(labels)
